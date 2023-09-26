3b4ff1840726b00884eeade31598aa0b38923cb7
Serhii Lypko <lypko.sg@gmail.com>
2023-09-26 12:08:41 +0800

    Better mapping approach for printing data

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,68 @@
 use std::fmt;
+use std::fs;
 use std::fs::{DirEntry, Metadata};
 use std::io::Result;
 use std::path::Path;
-use std::{fs, vec};
+
+// TODO: ordering of the structs & methods declaration?
+
+pub fn main() {
+    //
+}
 
 pub struct Ls;
 
-// TODO: formatting string so each column gows the same width in table
+impl Ls {
+    pub fn execute() -> Result<()> {
+        let arg_path_string = Ls::get_path_string_from_args();
+        let arg_path_string = match arg_path_string {
+            Some(path_string) => path_string,
+            None => ".".to_string(),
+        };
+
+        let root_path = Path::new(arg_path_string.as_str());
+
+        Ls::print_dir(&root_path)
+    }
+
+    // TODO: rename -> path_or_default?
+    fn get_path_string_from_args() -> Option<String> {
+        use std::env;
+
+        let args: Vec<String> = env::args().collect();
+
+        if args.len() > 1 {
+            let expected_arg_path = &args[1];
+            return Some(expected_arg_path.to_owned());
+        }
+
+        None
+    }
+
+    /*
+        fs::read_dir(path) returns Result<ReadDir, Error>
+        if success -> Iterator<Result<DirEntry, Error>>
+    */
+
+    // TODO: but how to implement sorting while mapping stuff
+    // TODO: simple strategy is to simply put dir on top of the new list and files on the end
+    fn print_dir(root_path: &Path) -> Result<()> {
+        use fs::read_dir;
+
+        // Iterator<Result<EntryInfo>>
+        let entries_info_iter = read_dir(root_path)?.map(EntryInfo::gather_data);
+
+        let res = entries_info_iter
+            .filter_map(Result::ok)
+            .fold("".to_string(), |cur, next| {
+                format!("{}{}\n", cur, next.info_string.as_str())
+            });
+
+        println!("{}", res);
+
+        Ok(())
+    }
+}
 
 enum EntryType {
     Dir,
@@ -36,64 +92,16 @@ impl From<Metadata> for EntryType {
     }
 }
 
-impl Ls {
-    pub fn execute() -> Result<()> {
-        let arg_path_string = Ls::get_path_string_from_args();
-        let arg_path_string = match arg_path_string {
-            Some(path_string) => path_string,
-            None => ".".to_string(),
-        };
+struct EntryInfo {
+    meta: Metadata,
+    info_string: String,
+}
 
-        let root_path = Path::new(arg_path_string.as_str());
-
-        Ls::print_dir(&root_path)
-    }
-
-    fn get_path_string_from_args() -> Option<String> {
-        use std::env;
-
-        let args: Vec<String> = env::args().collect();
-
-        if args.len() > 1 {
-            let expected_arg_path = &args[1];
-            return Some(expected_arg_path.to_owned());
-        }
-
-        None
-    }
-
-    fn print_dir(root_path: &Path) -> Result<()> {
-        use fs::read_dir;
-
-        let mut dirs_info: Vec<String> = vec![];
-        let mut files_info: Vec<String> = vec![];
-
-        for entry in read_dir(root_path)? {
-            if let Ok(entry) = entry {
-                if let Ok((metadata, entry_info)) = Ls::gather_entry_info(entry) {
-                    if metadata.is_dir() {
-                        dirs_info.push(entry_info);
-                    } else {
-                        files_info.push(entry_info);
-                    }
-                }
-            }
-        }
-
-        dirs_info.append(&mut files_info);
-
-        for dir_info in dirs_info {
-            println!("{}", dir_info);
-        }
-
-        Ok(())
-    }
-
-    // TODO: better to use Struct with named fields for Result T?
-    fn gather_entry_info(entry: DirEntry) -> Result<(Metadata, String)> {
+impl EntryInfo {
+    fn gather_data(entry: Result<DirEntry>) -> Result<Self> {
         use fs::metadata;
 
-        let path = entry.path();
+        let path = entry?.path();
         let metadata = metadata(&path)?;
 
         let name = path
@@ -103,12 +111,11 @@ impl Ls {
 
         let base_type: EntryType = metadata.clone().into();
 
-        let entry_info = format!("{} {} {}", name, base_type, metadata.len());
+        let info_string = format!("{} {} {}", name, base_type, metadata.len());
 
-        Ok((metadata, entry_info))
+        Ok(EntryInfo {
+            meta: metadata,
+            info_string,
+        })
     }
-}
-
-fn main() -> Result<()> {
-    Ls::execute()
 }
